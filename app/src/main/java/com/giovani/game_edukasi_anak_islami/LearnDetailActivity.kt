9e8e3d4cb412847b4307afcb2eefbc8397cd7c37
Recky Anderson <reckyanderson@gmail.com>
2022-06-04 21:51:29 +0700

    Adding arabic vocal sound functionality to LearnDetailActivity.kt

--- app/src/main/java/com/giovani/game_edukasi_anak_islami/LearnDetailActivity.kt
+++ app/src/main/java/com/giovani/game_edukasi_anak_islami/LearnDetailActivity.kt
@@ -4,50 +4,11 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.giovani.game_edukasi_anak_islami.data.ArabicVocal
 
 class LearnDetailActivity : AppCompatActivity() {
-    private data class ArabicVocal(val fileName: String, val arabic: String)
 
-    private val arabicVocalList = mutableListOf(
-        ArabicVocal("belajar_satu", "١"),
-        ArabicVocal("belajar_dua", "٢"),
-        ArabicVocal("belajar_tiga", "٣"),
-        ArabicVocal("belajar_empat", "٤"),
-        ArabicVocal("belajar_lima", "٥"),
-        ArabicVocal("belajar_enam", "٦"),
-        ArabicVocal("belajar_tujuh", "٧"),
-        ArabicVocal("belajar_delapan", "٨"),
-        ArabicVocal("belajar_sembilan", "٩"),
-        ArabicVocal("belajar_sepuluh", "١٠"),
-        ArabicVocal("belajar_alif", "ا"),
-        ArabicVocal("belajar_ba", "ب"),
-        ArabicVocal("belajar_ta", "ت"),
-        ArabicVocal("belajar_tsa", "ث"),
-        ArabicVocal("belajar_jim", "ج"),
-        ArabicVocal("belajar_hak", "ح"),
-        ArabicVocal("belajar_kho", "خ"),
-        ArabicVocal("belajar_dal", "د"),
-        ArabicVocal("belajar_dzal", "ذ"),
-        ArabicVocal("belajar_ra", "ر"),
-        ArabicVocal("belajar_zayn", "ز"),
-        ArabicVocal("belajar_sin", "س"),
-        ArabicVocal("belajar_syin", "ش"),
-        ArabicVocal("belajar_shad", "ص"),
-        ArabicVocal("belajar_dhad", "ض"),
-        ArabicVocal("belajar_tha", "ط"),
-        ArabicVocal("belajar_dzo", "ظ"),
-        ArabicVocal("belajar_ain", "ع"),
-        ArabicVocal("belajar_ghain", "غ"),
-        ArabicVocal("belajar_fa", "ف"),
-        ArabicVocal("belajar_khof", "ق"),
-        ArabicVocal("belajar_khaf", "ك"),
-        ArabicVocal("belajar_lam", "ل"),
-        ArabicVocal("belajar_mim", "م"),
-        ArabicVocal("belajar_nun", "ن"),
-        ArabicVocal("belajar_haa", "هـ"),
-        ArabicVocal("belajar_waw", "و"),
-        ArabicVocal("belajar_ya", "ي")
-    )
+    private lateinit var vocalSound: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,16 +27,68 @@ class LearnDetailActivity : AppCompatActivity() {
         txtV.text = data
         txtDesc.text = itemDesc
 
-        data?.let { playSound(it) }
+
+        data?.let {
+            playVocalSound(it).start()
+        }
+    }
+    override fun onBackPressed() {
+        if (vocalSound.isPlaying) {
+            vocalSound.stop()
+            vocalSound.reset()
+        }
+        super.onBackPressed()
     }
 
-    private fun playSound(arabic: String) {
-        val fileNameWithoutExtension: String = arabicVocalList.find { it.arabic == arabic }!!.fileName
-        val resId = resources.getIdentifier(fileNameWithoutExtension, "raw", packageName)
-        if (resId != 0) {
-            val mediaPlayer = MediaPlayer.create(applicationContext, resId)
-            mediaPlayer.start()
-        }
+
+    private fun playVocalSound(arabicText: String): MediaPlayer {
+        val listArabicVocal = listOf(
+            ArabicVocal("١", R.raw.belajar_satu),
+            ArabicVocal("٢", R.raw.belajar_dua),
+            ArabicVocal("٣", R.raw.belajar_tiga),
+            ArabicVocal("٤", R.raw.belajar_empat),
+            ArabicVocal("٥", R.raw.belajar_lima),
+            ArabicVocal("٦", R.raw.belajar_enam),
+            ArabicVocal("٧", R.raw.belajar_tujuh),
+            ArabicVocal("٨", R.raw.belajar_delapan),
+            ArabicVocal("٩", R.raw.belajar_sembilan),
+            ArabicVocal("١٠", R.raw.belajar_sepuluh),
+            ArabicVocal("ض", R.raw.belajar_dhad),
+            ArabicVocal("ش", R.raw.belajar_syin),
+            ArabicVocal("ص", R.raw.belajar_shad),
+            ArabicVocal("ظ", R.raw.belajar_dzo),
+            ArabicVocal("س", R.raw.belajar_sin),
+            ArabicVocal("ط", R.raw.belajar_tha),
+            ArabicVocal("ث", R.raw.belajar_tsa),
+            ArabicVocal("ي", R.raw.belajar_ya),
+            ArabicVocal("ذ", R.raw.belajar_dzal),
+            ArabicVocal("ق", R.raw.belajar_khof),
+            ArabicVocal("ب", R.raw.belajar_ba),
+            ArabicVocal("د", R.raw.belajar_dal),
+            ArabicVocal("ف", R.raw.belajar_fa),
+            ArabicVocal("ل", R.raw.belajar_lam),
+            ArabicVocal("ز", R.raw.belajar_zayn),
+            ArabicVocal("غ", R.raw.belajar_ghain),
+            ArabicVocal("ا", R.raw.belajar_alif),
+            ArabicVocal("ر", R.raw.belajar_ra),
+            ArabicVocal("ع", R.raw.belajar_ain),
+            ArabicVocal("ت", R.raw.belajar_ta),
+            ArabicVocal("و", R.raw.belajar_waw),
+            ArabicVocal("هـ", R.raw.belajar_haa),
+            ArabicVocal("ن", R.raw.belajar_nun),
+            ArabicVocal("خ", R.raw.belajar_kho),
+            ArabicVocal("م", R.raw.belajar_mim),
+            ArabicVocal("ح", R.raw.belajar_hak),
+            ArabicVocal("ك", R.raw.belajar_khaf),
+            ArabicVocal("ج", R.raw.belajar_jim)
+        )
+        vocalSound = MediaPlayer.create(
+            applicationContext,
+            listArabicVocal.find { it.arabic == arabicText }!!.rawInt
+        )
+        vocalSound.isLooping = false
+
+        return this.vocalSound
     }
 
 }
