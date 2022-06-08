0f31c64e2ddf7fafdae3df4973b722a58dad5cb3
Recky Anderson <reckyanderson@gmail.com>
2022-06-08 14:53:00 +0700

    Implement shared preference change listener to detect volume control

--- app/src/main/java/com/giovani/game_edukasi_anak_islami/BackgroundMusic.kt
+++ app/src/main/java/com/giovani/game_edukasi_anak_islami/BackgroundMusic.kt
@@ -2,23 +2,29 @@ package com.giovani.game_edukasi_anak_islami
 
 import android.app.Service
 import android.content.Intent
+import android.content.SharedPreferences
 import android.media.MediaPlayer
 import android.os.IBinder
+import androidx.preference.PreferenceManager
 
-class BackgroundMusic : Service() {
+class BackgroundMusic : Service(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     private lateinit var bgMusic: MediaPlayer
 
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
+
     override fun onCreate() {
         super.onCreate()
-        bgMusic = MediaPlayer.create(applicationContext,R.raw.backgroundmusic)
+        var prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        prefs.registerOnSharedPreferenceChangeListener(this)
+        bgMusic = MediaPlayer.create(applicationContext, R.raw.backgroundmusic)
         bgMusic.isLooping = true // Set looping
         bgMusic.setVolume(0.5f, 0.5f)
 
     }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         bgMusic.start()
         return START_STICKY
@@ -33,6 +39,12 @@ class BackgroundMusic : Service() {
 
     }
 
+    override fun onSharedPreferenceChanged(sp: SharedPreferences, s: String) {
+        if (s == "bgVolume") {
+            var bgVolume: Float = (sp.getInt(s, 50).toFloat()) / 100
+            bgMusic.setVolume(bgVolume, bgVolume)
+        }
+    }
 
 
 }
