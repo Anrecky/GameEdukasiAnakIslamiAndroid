c676bc60f501e662df0e3598072d4f1fc19f807b
Recky Anderson <reckyanderson@gmail.com>
2022-06-08 14:47:31 +0700

    Delete not used import and delete syntax for action bar

--- app/src/main/java/com/giovani/game_edukasi_anak_islami/AboutFragment.kt
+++ app/src/main/java/com/giovani/game_edukasi_anak_islami/AboutFragment.kt
@@ -1,14 +1,10 @@
 package com.giovani.game_edukasi_anak_islami
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.MaterialToolbar
-
+import androidx.fragment.app.Fragment
 
 class AboutFragment : Fragment() {
     private lateinit var root: View
@@ -18,11 +14,6 @@ class AboutFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.fragment_about, container, false)
-//        val toolbar = root.findViewById<MaterialToolbar>(R.id.topAppBar)
-//        (activity as AppCompatActivity).setSupportActionBar(toolbar)
-//        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        (activity as AppCompatActivity).supportActionBar?.setHomeButtonEnabled(true)
-
         return root
     }
 
