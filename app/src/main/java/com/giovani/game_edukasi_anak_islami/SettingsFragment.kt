d1b3b55f66a8ebbd471cc2f44d790b9a83fd33ca
Recky Anderson <reckyanderson@gmail.com>
2022-06-08 15:11:27 +0700

    Change SettingsFragment.kt (Fragment) to PreferenceFragmentCompat()

--- app/src/main/java/com/giovani/game_edukasi_anak_islami/SettingsFragment.kt
+++ app/src/main/java/com/giovani/game_edukasi_anak_islami/SettingsFragment.kt
@@ -1,18 +1,11 @@
 package com.giovani.game_edukasi_anak_islami
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
+class SettingsFragment : PreferenceFragmentCompat() {
 
-class SettingsFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
