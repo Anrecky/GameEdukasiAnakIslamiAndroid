231cba19c3210b752f3d62ef14f21f2221f2ac38
Recky Anderson <reckyanderson@gmail.com>
2022-06-08 15:09:10 +0700

    Delete implementation shared preferences on change listener and start single line service

--- app/src/main/java/com/giovani/game_edukasi_anak_islami/MainActivity.kt
+++ app/src/main/java/com/giovani/game_edukasi_anak_islami/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.giovani.game_edukasi_anak_islami
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.giovani.game_edukasi_anak_islami.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val bgMusicIntentService = Intent(this,BackgroundMusic::class.java)
-        startService(bgMusicIntentService)
+        Intent(this, BackgroundMusic::class.java).also {
+            startService(it)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
     }
+
 }
