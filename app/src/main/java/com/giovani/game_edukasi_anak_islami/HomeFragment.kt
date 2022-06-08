fc88a7bbcfddbaea01011ff683047b7de40ec2de
Recky Anderson <reckyanderson@gmail.com>
2022-06-08 15:00:03 +0700

    Add navigate to fragmentContainerSettings

--- app/src/main/java/com/giovani/game_edukasi_anak_islami/HomeFragment.kt
+++ app/src/main/java/com/giovani/game_edukasi_anak_islami/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.giovani.game_edukasi_anak_islami
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.giovani.game_edukasi_anak_islami.R
 import com.giovani.game_edukasi_anak_islami.databinding.FragmentHomeBinding
 import kotlin.system.exitProcess
 
@@ -25,7 +24,7 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_homeFragment_to_startMenuFragment)
         }
         binding.settingsButton.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+            findNavController().navigate(R.id.action_homeFragment_to_fragmentContainerSettings)
         }
         binding.aboutButton.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_aboutFragment)
